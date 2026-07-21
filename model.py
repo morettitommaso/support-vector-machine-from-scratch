fb6a536d6588c705eeb489e098ba8fdf58086835
tommaso moretti <150902711+morettitommaso@users.noreply.github.com>
2026-07-21 18:39:29 +0200

    Step 10: predict_labels

--- model.py
+++ model.py
@@ -112,8 +112,14 @@ def train_svm(x, y, learning_rate, reg_lambda, n_epochs):
 
     return params
 
-# Step 10 - predict_labels (not yet solved)
-# TODO: implement
+# Step 10 - predict_labels
+import numpy as np
+
+def predict_labels(x, params):
+    # TODO: return an array of {-1, +1} labels, one per row of x, using params['w'] and params['b'].
+    
+    scores = compute_scores(x, params)
+    return predict_from_scores(scores)
 
 # Step 11 - accuracy_score (not yet solved)
 # TODO: implement
